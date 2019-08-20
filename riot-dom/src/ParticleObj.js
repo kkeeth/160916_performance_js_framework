9f7927d8491134dadb73ab0331ef067ea8e89f0f
k-kuwahara <zensin0082@gmail.com>
2019-08-20 20:50:14 +0900

    feat: create demo app about riot-dom

--- riot-dom/src/ParticleObj.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ParticleObj = ({particle}) =>
-  <div className="particle" style={{
-    top: particle.y + 'px',
-    left: particle.x + 'px',
-  }}>😊</div>;
-export default ParticleObj;
--- riot-dom/src/particle.js
+++ riot-dom/src/particle.js
@@ -3,36 +3,36 @@ const MAX_LIFE = 60; // 寿命の最大値
 /**
  * パーティクルの座標情報を管理するクラスです。
  */
-export default class ParticleData {
+export class ParticleData {
   constructor($x, $y, $key) {
-    this.x = $x;
-    this.y = $y;
-    this.key = $key;
-    this.vx = 0;
-    this.vy = 0;
-    this.life = 0;
+    this.x = $x
+    this.y = $y
+    this.key = $key
+    this.vx = 0
+    this.vy = 0
+    this.life = 0
 
     // 動的にプロパティーを追加します。
     // 速度
-    this.vx = 30 * (Math.random() - 0.5);
-    this.vy = 30 * (Math.random() - 0.5);
+    this.vx = 30 * (Math.random() - 0.5)
+    this.vy = 30 * (Math.random() - 0.5)
     // 寿命
-    this.life = MAX_LIFE;
+    this.life = MAX_LIFE
   }
 
   update() {
     // 重力
-    this.vy += 1;
+    this.vy += 1
     // 摩擦
-    this.vx *= 0.96;
-    this.vy *= 0.96;
+    this.vx *= 0.96
+    this.vy *= 0.96
     // 速度を位置に適用
-    this.x += this.vx;
-    this.y += this.vy;
+    this.x += this.vx
+    this.y += this.vy
 
     // 地面の跳ね返り処理
     if (this.y > window.innerHeight) {
-      this.y = window.innerHeight; // 行き過ぎ補正
+      this.y = window.innerHeight // 行き過ぎ補正
       this.vy *= -1; // Y軸の速度を反転
     }
 
